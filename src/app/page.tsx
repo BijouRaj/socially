65c0905cdd59233643d755ea8198b596dc00ac30
Bijou <bijou.raj12@gmail.com>
2025-07-02 20:24:59 -0700

    Initial commit, boilerplate and up to dark mode and account creation

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <h1>home page content</h1>
+    </div>
+  );
+}
